868655bc59a164ca9b88f6c0482d3ec05611a92d
Vikram Chowdhury <vikram@themetromaxgroup.com>
2022-12-20 16:27:53 +0530

    Pagination Working

--- src/components/company/CompanyListing.jsx
+++ src/components/company/CompanyListing.jsx
@@ -1,37 +1,27 @@
 import React, { useEffect, useState } from "react";
-import { useTable } from "react-table";
+import { usePagination, useTable } from "react-table";
 import { useMemo } from "react";
 import axios from "axios";
 import { getCompanyService } from "../../service/CompanyService/Company";
 import { authHeader } from "../../service/auth-headers";
 import { useLocation } from "react-router-dom";
-import { DataGrid } from '@mui/x-data-grid';
-
-
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { createNextState } from "@reduxjs/toolkit";
-import { tab } from "@testing-library/user-event/dist/tab";
-
 
 export default function CompanyListing() {
-  const [table, setTable] = useState([]);
+  const [data, setData] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [legalName, setLegalName] = useState("");
   const [name, setName] = useState("");
   const [dot, setDot] = useState("");
   const [city, setCity] = useState("");
+  const [localTable, setLocalTable] = useState();
   const [next, setNext] = useState();
   const [previous, setPrevious] = useState();
+  const [callNext,setNextCall]=useState(false)
+  const [callPrev,setPrevCall]=useState(false)
+
   // console.log(JSON.stringify(user.access))
   // const isLoggedIn = JSON.stringify(user.data)
 
-  const base_url = 'http://127.0.0.1:8000/api/company/'
 
   useEffect(()=>{
       console.log('inside')
@@ -50,61 +40,98 @@ export default function CompanyListing() {
   // Using useEffect to call the API once mounted and set the data
   useEffect(() => {
     (async () => {
-      const res = await getCompanyService();
-      let temp = flatData(res.data.results)
-      setTable(temp);
-      if(res?.data?.next){
-        setNext(res.data.next)
+      const result = await getCompanyService();
+      let temp = flatData(result.data.results);
+      setData(temp)
+      // assigning next of url in next var
+      if (result?.data?.next){
+        setNext(result.data.next)
       }
-      if(res?.data?.previous){
-        setPrevious(res.data.previous)
+      if (result?.data?.previous){
+        setPrevious(result.data.previous)
       }
-      console.log('first next: ', next, 'f',res.data.next)
     })();
   }, []);
-  console.log('firsttt page: ', next)
 
-
-  const handleChangePage = async () => {
-    console.log('going to the page: ', next)
-    const res = await axios.get(next, {headers: authHeader});
-    console.log('res.data: ',res.data)
-    if(res?.data?.next){
-      setNext(res.data.next)
-    }
-    if(res?.data?.previous){
-      setNext(res.data.previous)
-    }
-    let temp = flatData(res.data.results)
-    setTable(temp);
-  }
-  console.log('next page out: ', next)
+   
   
 
-  // destructing company data for the table
   const flatData = (companies) => {
+    console.log('flatData called" ', companies)
     let localCompanyList = [];
-    // let count = 1;
     for(let company of companies){
       let tempCompany = {};
-      // console.log(`first company ${company.legal_name}`)
-      
       tempCompany.id = company.id;
       tempCompany.legal_name = company.legal_name;
       tempCompany.dot = company.dot;
+      tempCompany.is_active = company.is_active;
       
       for(let address of company.addresses){
-        // console.log('address: ',address)
         tempCompany.city = address.city;
         tempCompany.state = address.state;
         tempCompany.zip = address.zip;
       }
       localCompanyList.push(tempCompany);
-      // break;
     }
-    // console.log('localCompanyList: ', localCompanyList)
+    console.log('localCompanyList: ',localCompanyList)
     return localCompanyList;
   }
+
+  const COLUMNS = [
+    {
+      Header: "Legal Name",
+      accessor: "legal_name",
+    },
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "DOT",
+      accessor: "dot",
+    },
+    {
+      Header: "City",
+      accessor: "city",
+    },
+    {
+      Header: "State",
+      accessor: "state",
+    },
+    {
+      Header: "MC Number",
+      accessor: "motor_carrier_number",
+    },
+    {
+      Header: "Incorporation date",
+      accessor: "incorporation_date",
+    },
+    {
+      Header: "Active",
+      accessor: "is_active",
+    },
+
+  ];
+  // THis code is very important if removed table will disappear
+  const columns = useMemo(() => COLUMNS, []);
+  // console.log(data);
+
+
+  
+    const {
+      getTableProps,
+      getTableBodyProps,
+      headerGroups,
+      prepareRow,
+      rows,
+      state: { pageIndex, pageSize },
+    } = useTable(
+      {
+        columns,
+        data,
+      }
+    )
+  
 
   const onChange = (e, field) => {
     const value = e.target.value;
@@ -115,43 +142,68 @@ export default function CompanyListing() {
       return axios.get(`http://127.0.0.1:8000/api/company/companies?legal_name=${legalName}&name=${name}&dot=${dot}&city=${city}`,  {headers: authHeader()})
   }
 
+
+// we created a state called callNext, when we need to change page we change the state of callpage, and give this as a dependency to useEffect
+  useEffect(()=>{
+    console.log("change the page")
+    console.log('test next: ', next)
+    const paginationHandler = async () => {
+      if (next!==undefined){
+        let response = await axios.get(next, {Header: authHeader})
+        let temp = flatData(response.data.results)
+        setData(temp)
+        if (response?.data?.next){
+          setNext(response.data.next)
+        }
+        if (response?.data?.previous){
+          setPrevious(response.data.previous)
+        }
+        console.log('response; ', response)
+      }
+    }
+    paginationHandler()
+  },[callNext])
+
+  useEffect(()=>{
+    console.log("change the page")
+    console.log('test next: ', next)
+    const paginationHandler = async () => {
+      if (previous!==undefined){
+        let response = await axios.get(previous, {Header: authHeader})
+        let temp = flatData(response.data.results)
+        setData(temp)
+        if (response?.data?.next){
+          setNext(response.data.next)
+        }
+        if (response?.data?.previous){
+          setPrevious(response.data.previous)
+        }
+        console.log('response; ', response)
+      }
+    }
+    paginationHandler()
+  },[callPrev])
+
+
+  
+
+
+//  console.log("ankit kumar",data)
+
   async function onsubmit(e){
     e.preventDefault();
     console.log('InsideCOmpany submit');
-    await filterlist(legalName, name, dot).
-    then((response)=>{
-      setTable(response.data)
+    await filterlist(legalName, name, dot)
+    .then((response)=>{
+      setData(response.data.results)
     })
   }
-  
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 100 },
-    { field: 'legal_name', headerName: 'Legal Name', width: 400 },
-    { field: 'dot', headerName: 'DOT', width: 100 },
-    { field: 'city', headerName: 'CITY', width: 100 },
-    { field: 'state', headerName: 'STATE', width: 100 },
-    // {
-    //   field: 'age',
-    //   headerName: 'Age',
-    //   type: 'number',
-    //   width: 90,
-    // },
-    // {
-    //   field: 'fullName',
-    //   headerName: 'Full name',
-    //   description: 'This column has a value getter and is not sortable.',
-    //   sortable: false,
-    //   width: 160,
-    //   valueGetter: (params) =>
-    //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    // },
-  ];
 
   return (
     <>
 
 
-    <div
+<div
       className="bg-light m-4 h-100 d-flex align-items-center justify-content-center p-2"
       style={{ "background-color": "#f7f7f7" }}
     >
@@ -193,9 +245,8 @@ export default function CompanyListing() {
             type="number"
             className="form-control"
             id="dot"
-            placeholder="ex. 12345"
             value={dot}
-            // placeholder='DOT'
+            placeholder='DOT'
             onChange={(e) => onChange(e, setDot)}
           />
         </div>
@@ -207,9 +258,8 @@ export default function CompanyListing() {
             type="text"
             className="form-control"
             id="city"
-            placeholder="ex. 12345"
             value={city}
-            // placeholder='City'
+            placeholder='City'
             onChange={(e) => onChange(e, setCity)}
           />
         </div>
@@ -219,53 +269,51 @@ export default function CompanyListing() {
       </form>
     </div>
 
-          {isLoggedIn && 
 
-            // <div style={{display: 'flex'}}>
-            //   <div style={{ height: '80vh', background: '#f7f7f7', flexGrow:1}} className='m-4 mt-1'>
-            //     <DataGrid
-            //       rows={localTable}
-            //       columns={columns}
-            //     />
-            //   </div>
-            // </div>
 
-            <div className='m-4'>
-              <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell className='font-weight-bold'>DOT</TableCell>
-                      <TableCell className='font-weight-bold'>Legal Name</TableCell>
-                      <TableCell className='font-weight-bold'>City</TableCell>
-                      <TableCell className='font-weight-bold'>State</TableCell>
+     {/* listing */}
+      {isLoggedIn?
+      <div className="bg-light m-4 h-100 d-flex align-items-center justify-content-center">
+        <br />
+        <table {...getTableProps()} className="table m-3">
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()} style={{ color: "#ff6600" }}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+              <div style={{width:'100'}} className='pagination m-1'>
+              <nav aria-label="Page navigation example">
+                  <button onClick={() =>setPrevCall(pre=>!pre)}>Previous</button>
+                </nav>
+                <nav aria-label="Page navigation example">
+                  <button onClick={() =>setNextCall(pre=>!pre)}>Next</button>
+                </nav>
 
-                      {/* <TableCell align="right">Calories</TableCell>
-                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                      <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {table.map((row) => (
-                      <TableRow
-                        key={row.name}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                      >
-
-                        <TableCell>{row.dot}</TableCell>
-                        <TableCell>{row.legal_name}</TableCell>
-                        <TableCell>{row.city}</TableCell>
-                        <TableCell>{row.state}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </div>
-
-          }
+              </div>
+        </table>
+      </div>
       
+    :'asdf'}
     </>
   );
 }
